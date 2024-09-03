431d338e3ae2ef50443738ef9953c87ba3ca85f0
eric.reyneke@sappi.com <eric.reyneke@sappo.com>
2024-09-03 17:38:23 +0200

    Commit temp and gitignore

--- Discord/Business.DiscordBlazor/obj/Debug/net8.0/Business.DiscordBlazor.AssemblyInfo.cs
+++ Discord/Business.DiscordBlazor/obj/Debug/net8.0/Business.DiscordBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business.DiscordBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738615f7cc4090075211e9bbc69c7efb44fb9120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f3fca848767df558487ca9f727442645f98b40")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business.DiscordBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business.DiscordBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
