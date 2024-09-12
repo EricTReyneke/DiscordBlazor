caeff27e6860e6ef47c901a52afcf103b61ebcc6
EricTReyneke <eric2001reyneke@gmail.com>
2024-09-12 19:26:39 +0200

    Added the connection services for the Database Apis. Added Sweet Alerts.

--- Discord/DiscordBlazor/Program.cs
+++ Discord/DiscordBlazor/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+using Blazored.Toast;
 using DiscordBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,6 +8,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+#region Injections
+builder.Services.AddBlazoredToast();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(sp =>
+{
+    return new DiscordDataOperations("https://localhost:7299/swagger/v1/swagger.json", sp.GetRequiredService<HttpClient>());
+});
+
+builder.Services.AddBlazoredLocalStorage();
+#endregion
 
 await builder.Build().RunAsync();
